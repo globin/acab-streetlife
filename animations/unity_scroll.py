6131f5288a79b5081c946df9adb062a06673fda6
Markus Teich <markus.teich@stusta.mhn.de>
2013-04-07 21:45:24 +0200

    add unity scroll animation

---
+++ animations/unity_scroll.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import Image
+import time
+from acabsl import *
+
+# setup
+img = Image.open("unity.png")
+img_width = img.size[0]
+img_raw = img.getdata()
+
+for wall in range(NOOFWALLS):
+    for col in range(WALLSIZEX):
+        for row in range(WALLSIZEY):
+            send(wall,col,row,0,0,0,0)
+update()
+
+
+def render_frame(data, shift):
+    ptr = 0
+    for wall in range(NOOFWALLS):
+        for y in range(WALLSIZEY):
+            for tx in range(WALLSIZEX):
+                ix = (tx + shift) % img_width
+                ptr = ix + y * img_width
+                if type(data[ptr]) == int:
+                    send(wall,tx,y,data[ptr],data[ptr],data[ptr]);
+                else:
+                    send(wall,tx,y,data[ptr][0],data[ptr][1],data[ptr][2]);
+    update()
+
+
+while True:
+    for shift in range(img_width):
+        render_frame(img_raw, shift)
+        time.sleep(0.3)
