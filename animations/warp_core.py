ed6db7d11a4124b3d5bdccb4b6eca30e9323a8e6
Markus Teich <markus.teich@stusta.mhn.de>
2013-04-07 22:25:48 +0200

    fix warp core indentation

--- animations/warp_core.py
+++ animations/warp_core.py
@@ -13,22 +13,22 @@ b = 250
 shade = 3
 
 def blank_walls():
-  for col in range(WALLSIZEX):
-    for row in range(WALLSIZEY):
-      for wall in range(NOOFWALLS):
-        send(wall,col,row,0,0,0,0);
-  update()
+    for col in range(WALLSIZEX):
+        for row in range(WALLSIZEY):
+            for wall in range(NOOFWALLS):
+                send(wall,col,row,0,0,0,0);
+    update()
 
 def warp_ring(wall, row, time):
-  for i in range(WALLSIZEX):
-    if row < WALLSIZEY - 1: 
-      send(wall,i,row+1,r/shade,g/shade,b/shade,time);
-    if row <= WALLSIZEY - 1:
-      send(wall,i,row,r,g,b,time);
-    if row > 0 and row <= WALLSIZEY:
-      send(wall,i, row-1,r/shade,g/shade,b/shade,time);
+    for i in range(WALLSIZEX):
+        if row < WALLSIZEY - 1: 
+            send(wall,i,row+1,r/shade,g/shade,b/shade,time);
+        if row <= WALLSIZEY - 1:
+            send(wall,i,row,r,g,b,time);
+        if row > 0 and row <= WALLSIZEY:
+            send(wall,i, row-1,r/shade,g/shade,b/shade,time);
 
-  update()
+    update()
 
 
 c = 0
@@ -36,14 +36,12 @@ c = 0
 blank_walls()
 
 while 1:
-  for i in range(0,NOOFWALLS):
-    warp_ring(i, c, tick)
+    for i in range(0,NOOFWALLS):
+        warp_ring(i, c, tick)
 
-  c += 1
-  c = c % (WALLSIZEY + 2)
+    c += 1
+    c = c % (WALLSIZEY + 2)
 
-  time.sleep(tick)
-  
-  blank_walls()
+    time.sleep(tick)
 
-
+    blank_walls()
